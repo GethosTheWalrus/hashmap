194660bea12be2102a877abeb32867893cfe0b7c
Mike Toscano <michael.andrew.toscano@gmail.com>
2018-09-10 23:29:53 -0400

    Updating names

--- src/com/toscano/main/Main.java
+++ src/com/toscano/main/Main.java
@@ -9,14 +9,15 @@ public class Main {
 		HashMap<Integer,Hero> h = new HashMap<Integer,Hero>();
 		
 		h.Put( 0, new Hero("Grommash Hellscream", "Orc", "Blademaster") );
-		h.Put( 1, new Hero("Thrall", "Orc", "Farseer") );
+		h.Put( 1, new Hero("Thrall son of Durotan", "Orc", "Farseer") );
 		h.Put( 2, new Hero("Jaina Proudmoore", "Human", "Frost Mage") );
 		h.Put( 3, new Hero("Arthas Menethil", "Undead", "Deathknight") );
-		h.Put( 4, new Hero("Thalyssra", "Nightborne", "Arcanist") );
-		h.Put( 5, new Hero("Sylvanas Windrunner", "Undead", "Dark Archer") );
-		h.Put( 6, new Hero("Illadin Stormrage", "Night Elf", "Demon Hunter") );
-		h.Put( 7, new Hero("Kael'thas", "Blood Elf", "Fire Mage") );
-		h.Put( 8, new Hero("Aggramar", "Celestial", "Titan") );
+		h.Put( 4, new Hero("Ner'Zhul", "Orc", "Lich King") );
+		h.Put( 5, new Hero("Thalyssra", "Nightborne", "Arcanist") );
+		h.Put( 6, new Hero("Sylvanas Windrunner", "Undead", "Dark Archer") );
+		h.Put( 7, new Hero("Illadin Stormrage", "Night Elf", "Demon Hunter") );
+		h.Put( 8, new Hero("Kael'thas Sunstrider", "Blood Elf", "Blood Mage") );
+		h.Put( 9, new Hero("Aggramar", "Celestial", "Titan") );
 								
 		for( int i = 0; i < h.size; i++ ) {
 			
